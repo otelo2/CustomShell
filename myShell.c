ac630f4bbf729ae7b6e61efa9c6dcdb5b6fe5b33
Otelo2 <josean2009@live.com.mx>
2020-03-09 21:19:18 -0400

    Exit and & working, everything else broken TODO: Fix multiple children

--- myShell.c
+++ myShell.c
@@ -7,11 +7,12 @@
 int main(void)
 {
     char *args[MAX_LINE / 2 + 1]; //command line arguments
-    int should_run = 1;           //flag to determine when to end program
+    int should_run, firstRun = 1;           //flag to determine when to end program
     pid_t pidC1;
     char temp[MAX_LINE / 2 + 1];
     int numOfArgs, i = 0;
-    pidC1 = fork();
+    
+    //pidC1 = fork();
 
     while (should_run)
     {
@@ -26,35 +27,44 @@ int main(void)
         }
         numOfArgs = i;
         //Code to see what arguments we have (debugging)
-        /*
+        
         //printf("%d\n",numOfArgs);
         for (i = 0; i < numOfArgs; i++)
         {
             printf("%s",args[i]);
             printf("\n");
         } 
+
+        //1. Fork child process with fork()
+        //pidC1 = fork();
+        if (firstRun=1)
+        {
+            firstRun=0; //asures we only get one fork
+            pidC1=fork();
+        }
+
+        //check for exit command
         if (strcmp("exit\n", args[0]) == 0)
         {
             should_run=0;
             return 0;
         }
-        */
-        //1. Fork child process with fork()
-        //pidC1 = fork();
-
+        
+        //Standard child handling
         if (pidC1 < 0)
         {
             fprintf(stderr, "Fork failed");
         }
         else if (pidC1 == 0)
         {
-            printf("Child ID: %d\n", getpid());
+            //printf("Child ID: %d\n", getpid());
+            //This line can't be modified according to the practice.
             execvp(args[0], args);
         }
         else if (strcmp("&\n", args[numOfArgs - 1]) == 0)
         {
-            wait(NULL);
             printf("Got here because of a &\n");
+            wait(NULL);
         }
         //2. Child will invoce execvp()
         //3. If command included &, parent will wait();
